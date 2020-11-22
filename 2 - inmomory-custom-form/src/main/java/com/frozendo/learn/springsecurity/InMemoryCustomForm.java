965c636f36e4edd741881478e18c7fcc2ac79201
Flavio Rozendo <flavio.ap.rozendo@gmail.com>
2020-11-22 12:50:53 -0300

    Separete examples in different projects

---
+++ 2 - inmomory-custom-form/src/main/java/com/frozendo/learn/springsecurity/InMemoryCustomForm.java
@@ -0,0 +1,13 @@
+package com.frozendo.learn.springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InMemoryCustomForm {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InMemoryCustomForm.class, args);
+	}
+
+}
